4c62991897e0ef80540f956fdadfdee75b2a8059
Toni Sjö <amadot83@gmail.com>
2022-09-06 09:27:07 +0300

    6.9

---
+++ react/04_forms/src/nameform.js
@@ -0,0 +1,40 @@
+import {useState} from 'react';
+
+const NameForm = () => {
+
+    const [state,setState] = useState({
+        firstname:"",
+        lastname:""
+    })
+
+    const onChange = (event) => {
+        setState((state) => {
+            return {
+                ...state,
+                [event.target.name]:event.target.value
+            }
+        })
+    }
+    
+
+    const onSubmit = (event) => {
+        event.preventDefault();
+        let name = state.firstname + " " + state.lastname;
+        props.setGreeting(name);
+        
+    }
+
+    return (
+        <form onSubmit={onSubmit}>
+            <label htmlFor="firstname">First name:</label>
+            <input type="text" name="firstname" id="firstname" value={state.firstname} onChange={onChange}/>
+            <br/>
+            <label htmlFor="lastname">Last name:</label>
+            <input type="text" name="lastname" id="lastname" value={state.lastname} onChange={onChange}/>
+            <br/>
+            <input type="submit" value="Greet"/>
+        </form>
+    )
+}
+
+export default NameForm;
